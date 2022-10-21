b7e07fe207379692a5b0e69c1864da8483b7a305
uranxo <98714809+uranxo07@users.noreply.github.com>
2022-10-21 15:58:49 +0200

    Delete ciao.bat

--- ciao.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\%~n0.lnk');$s.TargetPath='%~f0';$s.Save()"
-cls
-echo ciao
-pause
-exit
